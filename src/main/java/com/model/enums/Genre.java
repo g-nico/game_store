09cb9bacbf59a9e57cdc5cdb622bbc90c0fab625
nicoleta.ghitescu <nicoleta.ghitescu@pitechnologies.ro>
2018-04-27 09:43:58 +0300

    Start working on the front-end.

--- src/main/java/com/model/enums/Genre.java
+++ src/main/java/com/model/enums/Genre.java
@@ -1,8 +1,8 @@
 package com.model.enums;
 
 public enum Genre {
-    ACTION,
-    ADVENTURE,
-    ROLE_PLAY,
-    SHOOTER
+    ACTION, //0
+    ADVENTURE, //1
+    ROLE_PLAY, //2
+    SHOOTER //3
 }
