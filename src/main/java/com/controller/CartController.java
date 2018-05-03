4f1124e5ff5905b60b9a7d76adce30013750b6aa
nicoleta.ghitescu <nicoleta.ghitescu@pitechnologies.ro>
2018-05-03 18:02:57 +0300

    Start working on carts

--- src/main/java/com/controller/CartController.java
+++ src/main/java/com/controller/CartController.java
@@ -1,6 +1,7 @@
 package com.controller;
 
 import com.model.frontObjects.CartDto;
+import com.model.frontObjects.GameDto;
 import com.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,7 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Controller
@@ -18,22 +21,24 @@ public class CartController {
     private CartService cartService;
 
     @GetMapping(value = "myCart")
-    public String addToMyCart(@CookieValue("gameIds") String cookie, Model model) {
+    public @ModelAttribute("cartDto") CartDto addToMyCart(@CookieValue("gameIds") String cookie, Model model) {
         String[] ids = cookie.split(",");
         List<Long> ids1;
 
         ids1 = Arrays.stream(ids)
+                .map(id -> {
+                    id.replaceAll("\\s", "");
+                    return id;
+                })
                 .map(Long::new)
                 .collect(Collectors.toList());
-        model.addAttribute("cartDto", cartService.getCart(ids1));
-
-        return "myCart";
+        return cartService.getCart(ids1);
     }
 
     @DeleteMapping(value = "myCart/delete/{name}")
-    public String deleteFromCart(@PathVariable Long id,
+    public String deleteFromCart(@PathVariable String name,
                                  @ModelAttribute(value = "cartDto") CartDto cartDto) {
-        cartService.delete(id, cartDto);
+        //cartService.delete(name, games);
 
         return "redirect:/myCart";
     }
