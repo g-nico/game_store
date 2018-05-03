44a63b5125214b352bfb1c7b079efd7f193f588e
nicoleta.ghitescu <nicoleta.ghitescu@pitechnologies.ro>
2018-05-03 14:49:36 +0300

    Start working on carts

--- src/main/java/com/service/CartService.java
+++ src/main/java/com/service/CartService.java
@@ -18,14 +18,32 @@ public class CartService {
 
     public CartDto getCart(final List<Long> ids) {
         CartDto cartDto = new CartDto();
+        boolean found = false;
 
-        cartDto.setGames(ids.stream()
-                .collect(Collectors.groupingBy(gameService::getOne, Collectors.counting())));
+        for(Long id : ids) {
+            GameDto g = gameService.getOne(id);
+            for(GameDto gd: cartDto.getGames().keySet()) {
+                if (gd.getId().equals(id)) {
+                    found = true;
+                    Long val = cartDto.getGames().get(gd) + 1;
+                    cartDto.getGames().put(gd, val);
+                }
+            }
+            if(!found) {
+                cartDto.getGames().put(g, 1L);
+            }
+            found = false;
+        }
 
         return cartDto;
     }
 
-    public void delete(Long id, CartDto cartDto) {
-        //cartDto.getGames().remove(gameDto);
+    public void delete(String name, CartDto cartDto) {
+        for(GameDto g : cartDto.getGames().keySet()) {
+            if (name.equals(g.getName())) {
+                cartDto.getGames().remove(g);
+                break;
+            }
+        }
     }
 }
